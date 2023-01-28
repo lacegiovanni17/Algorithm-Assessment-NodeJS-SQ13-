b1ff3579b1e994aaadf15bfdddaa4dbe7c2a6012
Chidike Henry <dk1online25@gmail.com>
2023-01-28 23:59:51 +0100

    added new feat to week 7 file

--- Week7/arrayOfProducts.js
+++ Week7/arrayOfProducts.js
@@ -29,3 +29,18 @@ function arrayOfProducts(arr) {
   }
   return result;
 }
+
+// // Solution 2
+function arrayOfProducts(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let product = 1;
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        product *= arr[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+}
