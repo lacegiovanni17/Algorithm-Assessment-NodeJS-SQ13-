735ab4260f966241ac30e26b749b8f059b19915c
Chidike Henry <dk1online25@gmail.com>
2023-02-22 17:26:23 +0100

    added new feat to week10

---
+++ Week11/binaryTreeCompare.js
@@ -0,0 +1,26 @@
+// write the answer in JS
+// Task
+// Given the following class:
+
+// class TreeNode {
+//   constructor(val, left=null, right=null) {
+//     this.val = val;
+//     this.left = left;
+//     this.right = right;
+//   }
+// }
+// complete the function
+
+// const binaryTreeCompare = (a, b) => {};
+// which compares the two trees defined by TreeNodes a and b and returns true if they are equal in structure and in value and false otherwise.
+
+// For example:
+
+//solution 1
+const binaryTreeCompare = (a, b) => {
+    if (a === null && b === null) return true;
+    if (a === null || b === null) return false;
+    return a.val === b.val && binaryTreeCompare(a.left, b.left) && binaryTreeCompare(a.right, b.right);
+}
+
+//solution 2
